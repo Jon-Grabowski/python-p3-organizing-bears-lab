5e39424ede9e790408af0d6d3615d48d4a38f2b9
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-13 13:10:17 -0400

    All tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ("Mr. Chocolate", 5, "M", "Brown", "chill", 1),
+    ("Rowdy", 3, "M", "Brown", "rowdy", 1),
+    ("Tabitha", 6, "F", "Black", "posh", 1),
+    ("Sergeant Brown", 4, "M", "Brown", "militant", 0),
+    ("Melissa", 2, "F", "Black", "happy", 1),
+    ("Grinch", 5, "M", "Green", "grumpy", 0),
+    ("Wendy", 3, "F", "Brown", "playful", 1),
+    (NULL, 4, "M", "Black", "mysterious", 1)
+;
